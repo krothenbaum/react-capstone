5be5e5d3686580935d5659c69a88d25bbba9f4e9
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-05-21 14:20:25 -0700

    config

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ console.log(process.env.DATABASE_URL);
 console.log(global.DATABASE_URL);
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://reactadmin:345Ripley@ds149551.mlab.com:49551/react-capstone';
+                      'mongodb://localhost/mern-test';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://localhost/test-mern-test');                      
