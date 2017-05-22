769dbb071cbc2791e2a0921d32de4c9f6e9dfe8c
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-05-22 11:21:07 -0700

    Added Google map component

--- client/app/index.js
+++ client/app/index.js
@@ -11,17 +11,28 @@ import {
 import App from './components/App/App';
 import NotFound from './components/App/NotFound';
 
-import Home from './components/Home/Home';
+import SimpleMapExample from './components/Map/Map';
 
 import HelloWorld from './components/HelloWorld/HelloWorld';
 
 import './styles/styles.scss';
 
+// render((
+//   <Router>
+//     <App>
+//       <Switch>
+//         <Route exact path="/" component={Home}/>
+//         <Route path="/helloworld" component={HelloWorld}/>
+//         <Route component={NotFound}/>
+//       </Switch>
+//     </App>
+//   </Router>
+// ), document.getElementById('app'));
 render((
   <Router>
     <App>
       <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="/" component={SimpleMapExample}/>
         <Route path="/helloworld" component={HelloWorld}/>
         <Route component={NotFound}/>
       </Switch>
