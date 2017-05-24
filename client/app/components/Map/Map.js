1d16d0c87bdc9c669589346489a3cd6cc3873fd8
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-05-24 16:45:49 -0700

    Gelocation with marker

--- client/app/components/Map/Map.js
+++ client/app/components/Map/Map.js
@@ -5,51 +5,38 @@ import canUseDOM from "can-use-dom";
 import raf from "raf";
 
 import {
-  default as React,
-  Component,
+	default as React,
+	Component,
 } from "react";
 
 import {
-  withGoogleMap,
-  GoogleMap,
-  Circle,
-  InfoWindow,
+	withGoogleMap,
+	GoogleMap,
+	Marker,
 } from "react-google-maps";
 
 const geolocation = (
-  canUseDOM && navigator.geolocation ?
-  navigator.geolocation : 
-  ({
-    getCurrentPosition(success, failure) {
-      failure(`Your browser doesn't support geolocation.`);
-    },
-  })
+	canUseDOM && navigator.geolocation ?
+	navigator.geolocation : 
+	({
+		getCurrentPosition(success, failure) {
+			failure(`Your browser doesn't support geolocation.`);
+		},
+	})
 );
 
 const GeolocationExampleGoogleMap = withGoogleMap(props => (
-  <GoogleMap
-    defaultZoom={12}
-    center={props.center}
-  >
-    {props.center && (
-      <InfoWindow position={props.center}>
-        <div>{props.content}</div>
-      </InfoWindow>
-    )}
-    {props.center && (
-      <Circle
-        center={props.center}
-        radius={props.radius}
-        options={{
-          fillColor: `red`,
-          fillOpacity: 0.20,
-          strokeColor: `red`,
-          strokeOpacity: 1,
-          strokeWeight: 1,
-        }}
-      />
-    )}
-  </GoogleMap>
+	<GoogleMap
+		defaultZoom={12}
+		center={props.center}
+	>
+
+		<Marker
+			position={props.center}
+			title='You are (near) here.'
+		/>
+
+	</GoogleMap>
 ));
 
 /*
@@ -59,69 +46,53 @@ const GeolocationExampleGoogleMap = withGoogleMap(props => (
  */
 export default class GeolocationExample extends Component {
 
-  state = {
-    center: null,
-    content: null,
-    radius: 6000,
-  };
+	state = {
+		center: null,
+	};
 
-  isUnmounted = false;
+	isUnmounted = false;
 
-  componentDidMount() {
-    const tick = () => {
-      if (this.isUnmounted) {
-        return;
-      }
-      this.setState({ radius: Math.max(this.state.radius - 20, 0) });
+	componentDidMount() {
 
-      if (this.state.radius > 200) {
-        raf(tick);
-      }
-    };
-    geolocation.getCurrentPosition((position) => {
-      if (this.isUnmounted) {
-        return;
-      }
-      this.setState({
-        center: {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        },
-        content: `Location found using HTML5.`,
-      });
+		geolocation.getCurrentPosition((position) => {
+			if (this.isUnmounted) {
+				return;
+			}
+			this.setState({
+				center: {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+				}
+			});
 
-      raf(tick);
-    }, (reason) => {
-      if (this.isUnmounted) {
-        return;
-      }
-      this.setState({
-        center: {
-          lat: 60,
-          lng: 105,
-        },
-        content: `Error: The Geolocation service failed (${reason}).`,
-      });
-    });
-  }
+		}, (reason) => {
+			if (this.isUnmounted) {
+				return;
+			}
+			this.setState({
+				center: {
+					lat: 60,
+					lng: 105,
+				}
+			});
+		});
+	}
 
-  componentWillUnmount() {
-    this.isUnmounted = true;
-  }
+	componentWillUnmount() {
+		this.isUnmounted = true;
+	}
 
-  render() {
-    return (
-      <GeolocationExampleGoogleMap
-        containerElement={
-          <div style={{ height: `1000px` }} />
-        }
-        mapElement={
-          <div style={{ height: `1000px` }} />
-        }
-        center={this.state.center}
-        content={this.state.content}
-        radius={this.state.radius}
-      />
-    );
-  }
+	render() {
+		return (
+			<GeolocationExampleGoogleMap
+				containerElement={
+					<div style={{ height: `1000px` }} />
+				}
+				mapElement={
+					<div style={{ height: `1000px` }} />
+				}
+				center={this.state.center}
+			/>
+		);
+	}
 }
